392648354ce96c9eac750d5133df3663c4fa5e2f
Prateek-Mahawar <prateekmahawar@gmail.com>
2016-07-21 18:16:27 +0530

    Initial Commit

---
+++ date/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  date
+//
+//  Created by Prateek Mahawar on 21/07/16.
+//  Copyright © 2016 Prateek Mahawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
